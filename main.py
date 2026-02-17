20ad3f463f89395a1ae1302aff053db292d2a964
Varadraj Kamath <varadrajkamath66@gmail.com>
2026-02-17 22:12:36 +0530

    update

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/armstrong/<int:num>")
+def armstrong(num):
+    n = num
+    power = len(str(num))
+    total = 0
+
+    while n > 0:
+        digit = n % 10
+        total += digit ** power
+        n //= 10
+
+    if total == num:
+        print("Armstrong Number")
+        result ={
+            "Number":num,
+            "Armstrong":True,
+            "Server IP":"172.168.0.1"
+        } 
+    else:
+        print("Not an Armstrong Number")
+        result ={
+            "Number":num,
+            "Armstrong":False,
+            "Server IP":"172.168.0.1"
+        } 
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True)
