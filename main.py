810b90b27a902b40c7874a387a4a9d403a93acc7
Varadraj Kamath <varadrajkamath66@gmail.com>
2026-02-17 22:15:22 +0530

    jsonify added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
@@ -31,7 +31,7 @@ def armstrong(num):
             "Armstrong":False,
             "Server IP":"172.168.0.1"
         } 
-    return result
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
